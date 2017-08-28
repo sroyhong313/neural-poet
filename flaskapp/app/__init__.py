05fd5104291e0b629fcb2ad17aa72819fc4b87ef
Sung Jae Hong <sroyhong313@gmail.com>
2017-08-28 14:36:03 -0400

    add about page

--- flaskapp/app/__init__.py
+++ flaskapp/app/__init__.py
@@ -4,7 +4,8 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 import pymongo
-
+from jinja2 import evalcontextfilter, Markup, escape
+import re
 # local imports
 from config import app_config
 
@@ -14,13 +15,24 @@ mongodb = client["neuralpoet"]
 poem_col = mongodb["poems"]
 poem_col.create_index([("textfield", "text")])
 
+_paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
+
+
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
 
     Bootstrap(app)
-    # if user not logged in, display following message and redirect to following view
+
+    @app.template_filter()
+    @evalcontextfilter
+    def nl2br(eval_ctx, value):
+        result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n') \
+            for p in _paragraph_re.split(escape(value)))
+        if eval_ctx.autoescape:
+            result = Markup(result)
+        return result
 
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
@@ -38,3 +50,4 @@ def create_app(config_name):
         return render_template('errors/500.html', title='Internal Server Error'), 500
 
     return app
+
--- flaskapp/app/home/views.py
+++ flaskapp/app/home/views.py
@@ -4,34 +4,26 @@ from flask import abort, render_template, jsonify
 from flask_login import current_user, login_required
 from subprocess import check_output
 
+
+import app
 from app import poem_col
 from . import sample
 from . import home
 from .. import *
+
+
 
 @home.route('/')
 def homepage():
     """
     Render the homepage template on the / route
     """
-    return render_template('home/index.html', title="Welcome")
+    return render_template('home/index.html', title="Neural Poet Welcome")
 
-@home.route('/dashboard')
-@login_required
-def dashboard():
-    """
-    Render the dashboard template on the /dashboard route
-    """
-    return render_template('home/dashboard.html', title="Dashboard")
+@home.route('/about')
+def about():
+    return render_template('home/about.html', title='About Neural Poet')
 
-@home.route('/admin/dashboard')
-@login_required
-def admin_dashboard():
-    # prevent non-admins from accessing this page
-    if not current_user.is_admin:
-        abort(403)
-
-    return render_template('home/admin_dashboard.html', title="Dashboard-Admin")
 
 @home.route('/make', methods=['GET','POST'])
 def make():
@@ -51,5 +43,5 @@ def make():
 
     poem_col.insert({'textfield':poem})
     print (poem)
-    poem = poem.split('\n')
+    # poem = poem.split('\n')
     return render_template('home/new.html', title="New", poem=poem)
