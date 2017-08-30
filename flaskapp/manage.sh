f0b5f9ed8887abd5e701cbe65ea0136082ad5b12
Sung Jae Hong <sroyhong313@gmail.com>
2017-08-29 22:13:23 -0400

    reorganize, remove external training models:

--- flaskapp/manage.sh
+++
@@ -1,7 +0,0 @@
-export FLASK_CONFIG=development
-export FLASK_APP=run.py
-
-`#flask db init
-flask db migrate
-flask db upgrade`
-
--- run.py
+++ run.py
@@ -1,13 +1,10 @@
-from subprocess import check_output
-output = []
-while True:
-    command = ['python3', 'sample.py', '-n', '4000']
-    raw_text = check_output(command)
-    text = raw_text.decode('unicode_escape')
-    poem = text.split('\n\n\n\n')
-    if (len(poem) >= 3):
-        poem = poem[1]
-        break
+# run.py
 
-output.append({'text': poem})
-print(output)
+import os
+from app import create_app
+
+# config_name = os.getenv('FLASK_CONFIG')
+app = create_app('production')
+
+if __name__ == '__main__':
+    app.run()
